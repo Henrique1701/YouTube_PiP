2982cfe42e839d642c864082239daeb5f7565ec3
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2022-02-10 02:52:30 -0300

    Adiciona botão para carregar vídeo a partir de um link

--- YouTubePiP/ViewController.swift
+++ YouTubePiP/ViewController.swift
@@ -11,6 +11,18 @@ import youtube_ios_player_helper
 class ViewController: UIViewController {
     
     // MARK: - UI
+    private lazy var loadVideoButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.configuration = .borderedProminent()
+        button.setTitle("carregar vídeo", for: .normal)
+        let loadVideoAction = UIAction(handler: { [weak self] _ in
+            self?.tappedLoadVideoButton()
+        })
+        button.addAction(loadVideoAction, for: .touchUpInside)
+        return button
+    }()
+    
     private lazy var playerView: YTPlayerView = {
         let playerView = YTPlayerView(frame: .zero)
         playerView.translatesAutoresizingMaskIntoConstraints = false
@@ -22,9 +34,9 @@ class ViewController: UIViewController {
         let button = UIButton(frame: .zero)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.configuration = .borderedProminent()
-        button.setTitle("start pip", for: .normal)
+        button.setTitle("iniciar pip", for: .normal)
         let startPipAction = UIAction(handler: { [weak self] _ in
-            self?.playerView.pictureInPicture()
+            self?.tappedStartPipButton()
         })
         button.addAction(startPipAction, for: .touchUpInside)
         return button
@@ -41,6 +53,7 @@ class ViewController: UIViewController {
     private func setupHierarchyView() {
         view.addSubview(playerView)
         view.addSubview(startPipButton)
+        view.addSubview(loadVideoButton)
     }
     
     private func setupConstraints() {
@@ -50,10 +63,53 @@ class ViewController: UIViewController {
             playerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             playerView.heightAnchor.constraint(equalToConstant: view.bounds.width),
             
+            loadVideoButton.bottomAnchor.constraint(equalTo: playerView.topAnchor, constant: -32),
+            loadVideoButton.leadingAnchor.constraint(equalTo: playerView.leadingAnchor, constant: 16),
+            loadVideoButton.trailingAnchor.constraint(equalTo: playerView.trailingAnchor, constant: -16),
+            
             startPipButton.topAnchor.constraint(equalTo: playerView.bottomAnchor, constant: 32),
             startPipButton.leadingAnchor.constraint(equalTo: playerView.leadingAnchor, constant: 16),
             startPipButton.trailingAnchor.constraint(equalTo: playerView.trailingAnchor, constant: -16),
         ])
+    }
+    
+    private func tappedStartPipButton() {
+        playerView.pictureInPicture()
+    }
+    
+    private func tappedLoadVideoButton() {
+        showAlertView()
+    }
+    
+    private func showAlertView() {
+        let alert = UIAlertController(title: "Carregar Vídeo", message: "Copie e cole o link para o vídeo do YouTube que você deseja assistir", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: nil)
+        
+        let alertButton = UIAlertAction(title: "carregar", style: .default) { [weak self] _ in
+            guard let self = self,
+                  let textFields = alert.textFields,
+                  let link = textFields[0].text,
+                  let videoId = self.getVideoIdFrom(link) else { return }
+            self.loadVideoFromId(videoId)
+        }
+        
+        alert.addAction(alertButton)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func loadVideoFromId(_ id: String) {
+        playerView.load(withVideoId: id, playerVars: ["autoplay" : 1, "playsinline" : 1])
+    }
+    
+    private func getVideoIdFrom(_ link: String) -> String? {
+        if link.contains("www.youtube.com/embed/") || link.contains("youtu.be/") {
+            let linkComponents = link.components(separatedBy: "/")
+            return linkComponents.last
+        } else if link.contains("www.youtube.com/watch?v=") {
+            let linkComponents = link.components(separatedBy: "=")
+            return linkComponents.last
+        }
+        return nil
     }
 
 }
